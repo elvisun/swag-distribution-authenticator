ce8755a448efddb9b9355dd2518c463ebf8466aa
Elvis Sun <gmail@elvisun.com>
2018-12-20 23:46:22 -0500

    navigation after submit

--- app/lib/create_event.dart
+++ app/lib/create_event.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'models/routes.dart' as routes;
 class CreateEventWidget extends StatefulWidget {
   @override
   _CreateEventState createState() => _CreateEventState();
@@ -8,9 +8,14 @@ class CreateEventWidget extends StatefulWidget {
 
 class _CreateEventState extends State<CreateEventWidget> {
   final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
 
-  void _addToDb() async {
-    await Firestore.instance.collection('events').add({'name': _nameController.text});
+  Future<void> _addToDb() async {
+    await Firestore.instance.collection('events').add({
+      'name': _nameController.text,
+      'description': _descriptionController.text,
+    });
+    Navigator.pop(context);
   }
 
   @override
@@ -18,23 +23,36 @@ class _CreateEventState extends State<CreateEventWidget> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Register a new event'),
-      ),
-      body: Container(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: _nameController,
-              ),
-              MaterialButton(
-                onPressed: _addToDb,
-                color: Colors.blue,
-                textColor: Colors.white,
-                child: Text('submit'),
-              ),
-            ],
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.send),
+            onPressed: (() async => await _addToDb()),
           ),
-        ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(padding: EdgeInsets.all(20)),
+          ListTile(
+            leading: const Icon(Icons.event_note),
+            title: TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: "Event name",
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.all(20)),
+          ListTile(
+            leading: const Icon(Icons.note_add),
+            title: TextFormField(
+              controller: _descriptionController,
+              decoration: InputDecoration(
+                hintText: "description",
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
