7d4269a0c37933bb5b3f7a40440008bf01986de3
Elvis Sun <gmail@elvisun.com>
2018-12-29 17:23:14 -0500

    ignore npm modules

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,18 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    return res.redirect(303, snapshot.ref.toString());
+  });
+});
+
