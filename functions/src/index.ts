c3a681510ad7bf5ca1677ce46baf8e8cb007ef90
Elvis Sun <gmail@elvisun.com>
2018-12-29 22:13:12 -0500

    query all documents

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,54 @@
 import * as functions from 'firebase-functions';
-import * as firestore from 'firestore';
-
+import * as admin from 'firebase-admin';
+import * as math from 'mathjs';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
+
+admin.initializeApp();
+
+var testArray = [
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130,  127,  126,
+127,  125,  122,  135,  114,  122,  131,  130];
+
 export const helloWorld = functions.https.onCall((data, context) => {
 	// if(data.vector.length != 128) {
 	// 	throw new functions.https.HttpsError('invalid-argument', 'vector must have length of 128');
 	// }
+
 	console.log('hello world');
-	// firestore.doc('/face_vectors').getCollections.then(collections => {
-	// 	console.log(collections);
-	// });
-	return data;
+	var firestore = admin.firestore();
+	var allVectors = [];
+	return firestore.collection('face_vectors').select('vector').get().then(function(res) {
+			console.log(res.docs[0].get('vector'));
+			console.log(math.dot(testArray, res.docs[0].get('vector')));
+			return {
+				result: "hello"
+			};
+			// snapshot.forEach(function (doc) {
+			// 	console.log(doc.data().vector);
+			// 	allVectors.push(doc.data().vector);
+			// });
+			// console.log("items find:");
+			// console.log(allVectors.length);
+			// console.log(math.dot(allVectors[0],allVectors[1]));
+			// return {result: {
+			// 	length: allVectors.length,
+			// 	new: "hello",
+			// 	dot: math.dot(allVectors[0],allVectors[1])
+			// }};
+		}
+	);
 });
 
